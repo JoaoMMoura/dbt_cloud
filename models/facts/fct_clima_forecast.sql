6227be86680dd3cc237f97f5f7d526cd5e127fb7
João Moura <joaoluiz.mmoura@gmail.com>
2024-05-31 00:18:07 +0000

    Ajust to fct_clima_forecast

--- models/facts/fct_clima_forecast.sql
+++ models/facts/fct_clima_forecast.sql
@@ -6,9 +6,11 @@ WITH clima_forecast AS (
         LOCAL,
         MIN(TEMPERATURA_MINIMA) AS TEMPERATURA_MINIMA,
         MAX(TEMPERATURA_MAXIMA) AS TEMPERATURA_MAXIMA,
+        ROW_NUMBER() OVER (PARTITION BY {{ date_trunc("day", "DATA") }}, LOCAL ORDER BY LOCAL) AS row_num
     FROM {{ ref("dim_clima_forecast") }}
     GROUP BY {{ date_trunc("day", "DATA") }}, LOCAL
 )
+
 
 SELECT
     DIA,
@@ -16,3 +18,4 @@ SELECT
     TEMPERATURA_MINIMA,
     TEMPERATURA_MAXIMA
 FROM clima_forecast
+WHERE row_num = 1
